a1a275c25bb162f81c06903580a2fc9d774ad448
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-23 21:08:55 -0400

    Update Constants.java

--- PronounDB/src/main/java/io/github/juby210/acplugins/pronoundb/Constants.java
+++ PronounDB/src/main/java/io/github/juby210/acplugins/pronoundb/Constants.java
@@ -13,7 +13,7 @@ public final class Constants {
     public static final String WEBSITE = "https://pronoundb.org";
 
     public static class Endpoints {
-        public static String LOOKUP_BULK(Object[] ids) { return WEBSITE + "/api/v1/lookup-bulk?platform=discord&ids=" + TextUtils.join(",", ids); }
+        public static String LOOKUP_BULK(Object[] ids) { return WEBSITE + "/api/v2/lookup-bulk?platform=discord&ids=" + TextUtils.join(",", ids); }
     }
 
     private static final Map<String, List<String>> pronouns = new HashMap<>() {{
